91d342d4277b6a061cc62946f8bc8d0b24c388fa
Vinit <singhvinit435@gmail.com>
2024-05-13 01:12:22 +0530

    added account schema

--- db.js
+++ db.js
@@ -29,5 +29,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
 const User = mongoose.model("User", userSchema);
-module.exports = { User };
+const Account = mongoose.model("Account", accountSchema);
+module.exports = { User, Account };
